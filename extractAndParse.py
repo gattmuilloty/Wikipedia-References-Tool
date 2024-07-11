430027be5db2fa2ece0318b202581d3e80c72f55
Matthew Guilloty <mattguilloty@gmail.com>
2024-07-10 22:08:32 -0400

    Update file structure and modules

--- extractAndParse.py
+++ extractAndParse.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
 
 def getWikiReferences(pageName, contactEmail):
 
@@ -33,6 +34,9 @@ def getWikiReferences(pageName, contactEmail):
     df = pd.DataFrame(links) # Initialize data frame
 
     df.insert(0, 'reference', texts) # Insert reference text to data frame
+
+    if df.empty:
+        return 'Page does not exist!'
 
     return(df)
 
@@ -112,3 +116,71 @@ def googleSearch(query, APIkey, CSEid, numResults = 10):
     links = [item['link'] for item in searchResults.get('items', [])] # Get links of results
 
     return links
+
+
+def filterReferences(references):
+
+    '''
+    Return filtered reference section that only possesses problem URLs 
+
+    Args:
+        references (pandas.DataFrame): Data frame with Wikipedia reference data.
+
+    Returns: 
+        pandas.DataFrame: A dataframe containing the filtered reference data of the Wikipedia page.
+    '''
+
+    statusCodes = {
+    100: "Continue", 101: "Switching Protocols", 102: "Processing",
+    200: "OK", 201: "Created", 202: "Accepted", 203: "Non-Authoritative Information", 204: "No Content", 205: "Reset Content", 206: "Partial Content", 207: "Multi-Status", 208: "Already Reported", 226: "IM Used",
+    300: "Multiple Choices", 301: "Moved Permanently", 302: "Found", 303: "See Other", 304: "Not Modified", 305: "Use Proxy", 307: "Temporary Redirect", 308: "Permanent Redirect", 400: "Bad Request",
+    401: "Unauthorized", 402: "Payment Required", 403: "Forbidden", 404: "Not Found", 405: "Method Not Allowed", 406: "Not Acceptable", 407: "Proxy Authentication Required", 408: "Request Timeout", 409: "Conflict",
+    410: "Gone", 411: "Length Required", 412: "Precondition Failed", 413: "Payload Too Large", 414: "URI Too Long", 415: "Unsupported Media Type", 416: "Range Not Satisfiable", 417: "Expectation Failed", 418: "I'm a teapot", 421: "Misdirected Request", 
+    422: "Unprocessable Entity", 423: "Locked", 424: "Failed Dependency", 425: "Too Early", 426: "Upgrade Required", 428: "Precondition Required", 429: "Too Many Requests", 431: "Request Header Fields Too Large", 439: "Application Inactive", 451: "Unavailable For Legal Reasons", 
+    500: "Internal Server Error", 501: "Not Implemented", 502: "Bad Gateway", 503: "Service Unavailable", 504: "Gateway Timeout", 505: "HTTP Version Not Supported", 506: "Variant Also Negotiates", 507: "Insufficient Storage", 508: "Loop Detected", 510: "Not Extended", 511: "Network Authentication Required",
+    'ERR': 'Error'
+    }
+
+    referenceText = []
+    urls = []
+
+    for index, row in references.iterrows():
+        for i in range(len(row)):
+            if 'http' in str(row.iloc[i]):
+                referenceText.append(row.iloc[0])
+                urls.append(row.iloc[i])
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+
+    def get_status_code(url):
+        try:
+            response = requests.get(url, headers = headers, timeout = 5)
+            return response.status_code
+        except requests.exceptions.RequestException as e:
+            return 'ERR'
+    
+    statuses = []
+    description = []
+
+    for i in range(len(urls)):
+        status = get_status_code(urls[i])
+
+        print('URL ' + str(i + 1).rjust(3) + '/' + str(len(urls)) + ' : ' + str(status) + ' - ' + statusCodes[status])
+
+        statuses.append(status)
+        description.append(statusCodes[status])
+
+        time.sleep(1)
+
+    references = pd.DataFrame({
+        'reference': referenceText,
+        'URL': urls,
+        'status': statuses,
+        'description': description
+    })
+
+    codesToIgnore = [200, 201, 202, 206]
+
+    references = references[~references['status'].isin(codesToIgnore)].reset_index(drop=True)
+
+    return(references)
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from extractAndParse import *
+from exceptions import *
 
 def main():
-    print("Hello, world!")
+    wikiPageQuery = input('\nEnter Wikipedia page to query: ')
+
+    userContact = 'mattguilloty@gmail.com'
+
+    df = getWikiReferences(wikiPageQuery, userContact)
+
+    df = fixQuery(df, wikiPageQuery, userContact)
+
+    df = filterReferences(df)
+
+    print(df)
 
 if __name__ == "__main__":
     main()
